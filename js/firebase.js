0b1ab26369fd409a5780a06e86871935a0c4c0b5
Adrian Sikorski <adriangsikorski@gmail.com>
2026-06-18 11:51:25 -0400

    Move Firebase config out of main file

--- js/firebase.js
+++ js/firebase.js
@@ -2,15 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAln8N9nwUBlFMM1Pw-IygBZUyDEpiqvBQ",
-  authDomain: "website-17f5f.firebaseapp.com",
-  projectId: "website-17f5f",
-  storageBucket: "website-17f5f.firebasestorage.app",
-  messagingSenderId: "71041950507",
-  appId: "1:71041950507:web:45ad6eaa4073fc45a61b6e",
-  measurementId: "G-N19SS25B2F"
-};
+import { firebaseConfig } from "./firebaseConfig.js";
 
 const app = initializeApp(firebaseConfig);
 
